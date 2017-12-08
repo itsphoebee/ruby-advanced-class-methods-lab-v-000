64ef6d6951ca4172dace73ea765ae8aea1d87415
Phoebe Lau <phoebelau7@gmail.com>
2017-12-08 04:24:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
     song                  #returns the song
   end
 
-  def self.new_by_name(string)
-    song = self.new
+  def self.new_by_name(string)    
+    song = self.create
     song.name = string
     song
   end
